dcd6b87600843e6d2d15a6ae1ee30c0871e7fcd0
Hugo Sykes <hugo.sykes@infinityworks.com>
2020-03-14 16:50:29 +0000

    Adds first attempt at implementation with a few tests.

--- coin.py
+++ coin.py
@@ -1,5 +1,18 @@
+import itertools
 from typing import List
 
 
 def make_change(total: int, coins: List):
-    pass
+    if total == 0 or len(coins) == 0:
+        return 0
+
+    max_number_of_elements = int(total / min(coins))
+    combinations = []
+
+    for length in range(max_number_of_elements):
+        for combination in list(itertools.combinations_with_replacement(coins, length + 1)):
+            if sum(combination) == total:
+                combinations.append(combination)
+
+    return len(combinations)
+
--- test_coin.py
+++ test_coin.py
@@ -1,5 +1,37 @@
 from coin import make_change
 
 
-def test():
-    make_change(total=0, coins=[])
+def test_zero():
+    assert make_change(total=0, coins=[]) == 0
+
+
+def test_no_coins():
+    assert make_change(total=125, coins=[]) == 0
+
+
+def test_4__1_2():
+    assert make_change(total=4, coins=[1, 2]) == 3
+
+
+def test_5__1_2():
+    assert make_change(total=5, coins=[1, 2]) == 3
+
+
+def test_6__1_2():
+    assert make_change(total=6, coins=[1, 2]) == 4
+
+
+def test_6__2():
+    assert make_change(total=6, coins=[2]) == 1
+
+
+def test_10__1_2_5():
+    assert make_change(total=10, coins=[1, 2, 5]) == 10
+
+
+def test_10__3():
+    assert make_change(total=10, coins=[3]) == 0
+
+
+def test_10__1_3():
+    assert make_change(total=10, coins=[1, 3]) == 4
